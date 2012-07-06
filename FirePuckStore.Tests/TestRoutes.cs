24c9dfbecdfea15fc046e301a7d5cd9074cb4e93
Erix <spb.erix@gmail.com>
2012-07-06 13:23:09 +0400

    - test package added

---
+++ FirePuckStore.Tests/TestRoutes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+using FirePuckStore.Controllers;
+using Moq;
+using Xunit;
+using MvcContrib.TestHelper;
+
+namespace FirePuckStore.Tests
+{
+    public class TestRoutes
+    {
+        [Fact]
+        public void TestDefaultRoute()
+        {
+            var routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            "~/".ShouldMapTo<HomeController>(x => x.Index());
+        }
+    }
+}
