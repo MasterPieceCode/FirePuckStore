98f15b8408406bfdae0aa12cb897a9898877c4c5
Erix <spb.erix@gmail.com>
2012-07-20 19:38:31 +0400

    - Create Page Layout changed

---
+++ FirePuckStore/Scripts/ui.imageselector.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+
+    var image = $('#logo');
+    if (image.attr('src') == null || image.attr('src') == '') {
+        $('.editor-image').css('width', '300px');
+    }
+    else {
+        $('#uploadLabel').remove();
+        $('.editor-image').css('width', '');
+        $('#logo').css({ 'visibility': 'visible', 'height': '400px' });
+    }
+
+    $('.editor-image').mousedown(function (e) {
+        $('#fileInput').click();
+    });
+});
+
+function handleFiles(files) {
+    if (files.length == 0) {
+        return;
+    }
+    var file = files[0];        
+    if (!file.type.match('image.*')) {
+        return;
+    }
+    var fileReader = new FileReader();
+
+    fileReader.onload = (function (theFile) {
+        return function (e) {
+            $('#uploadLabel').remove();
+            $('.editor-image').css('width', '');
+            $('#logo').css({ 'visibility': 'visible', 'height': '400px' });
+            $('#logo').attr('src', e.target.result);
+            $('#filePath').text(theFile.name);
+        };
+    })(file);
+
+    fileReader.readAsDataURL(file);
+}
